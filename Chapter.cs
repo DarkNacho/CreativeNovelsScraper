8e0989ba5a11c235d36197454bfe34e9a4f1879a
Dark Nacho <dark_nacho_xd@hotmail.cl>
2021-07-01 10:40:00 -0400

    Agregar archivos de proyecto.

---
+++ Chapter.cs
@@ -0,0 +1,77 @@
+﻿using HtmlAgilityPack;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+namespace CreativeNovelsScraper
+{
+
+    class ContentJson
+    {
+        [JsonPropertyName("rendered")]
+        public string Rendered { get; set; }
+        [JsonPropertyName("protected")]
+        public bool IsProtected { get; set; } = false;
+    }
+    /// <summary>
+    /// Minimal wrapper of content for getting the full chapter
+    /// </summary>
+    class ChapterJson
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("link")]
+        public string Link { get; set; }
+        [JsonPropertyName("title")]
+        public ContentJson Title { get; set; } = new ContentJson();
+        [JsonPropertyName("slug")]
+        public string Slug { get; set; }
+        [JsonPropertyName("content")]
+        public ContentJson Content { get; set; } = new ContentJson();
+
+    }
+    class Chapter
+    {
+        private ChapterJson _chapter;
+        public Chapter(string link, string title = "")
+        {
+            _chapter = new ChapterJson();
+            Link = link;
+            Title = title;
+        }
+        public string Link { get => _chapter.Link ; set => _chapter.Link = value; }
+        public string Title { get => _chapter.Title.Rendered ; set => _chapter.Title.Rendered = value; }
+        public string Slug { get => _chapter.Slug ; }
+        public string Content { get => _chapter.Content.Rendered; }
+
+        /// <summary>
+        /// Get the real content of the chapter and parse it making it readable without a browser.
+        /// </summary>
+        /// <returns>The Chapter String Contnet</returns>
+        public string FillContentAndParse()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(Link).Result;
+                var links = response.Headers.TryGetValues("Link", out var values) ? values.ToArray() : null;
+                var l = Regex.Match(links[1], @"<(.*?)>").Value;
+                l = l.Substring(1, l.Length - 2);
+                
+                //ask for real content to api..
+                response = client.GetAsync(l).Result;
+                _chapter = JsonSerializer.Deserialize<ChapterJson>((response.Content.ReadAsStringAsync().Result));
+
+                /*
+                var doc = new HtmlDocument();
+                doc.LoadHtml(_chapter.Content.Rendered);
+                _chapter.Content.Rendered = doc.DocumentNode.InnerText;
+                */
+
+                return Content;
+                
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace CreativeNovelsScraper
+{
+
+    class Program
+    {
+        /// <summary>
+        /// Get all the chapter from "https://creativenovels.com/novel/*
+        /// </summary>
+        /// <param name="link">Link from the novel</param>
+        /// <returns>A list from all the chapters avalible for the novel ready to be filled and parsed</returns>
+        private static IEnumerable<Chapter> GetChapters(string link)
+        {
+            if (!Regex.IsMatch(link, @"https://creativenovels.com/novel/.*?")) throw new Exception("Bad link, make sure it is ok, example: https://creativenovels.com/novel/the-villains-need-to-save-the-world");
+
+            // Variables to be retrieved
+            string response = null;
+
+            HtmlWeb web = new HtmlWeb();
+            web.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36";
+            HtmlDocument doc = web.Load(link);
+            // Get Novel ID
+            HtmlNode idNode = doc.DocumentNode.SelectSingleNode("//div[@id='chapter_list_novel_page']");
+            string novelId = idNode.Attributes["class"].Value;
+
+            //Get Chapter List
+            using (var client = new HttpClient())
+                response = client.PostAsync("https://creativenovels.com/wp-admin/admin-ajax.php",
+                    new FormUrlEncodedContent(new[]
+                    {   new KeyValuePair<string, string>("action", "crn_chapter_list"),
+                        new KeyValuePair<string, string>("view_id", novelId)
+                    }
+                    )).Result.Content.ReadAsStringAsync().Result;
+
+            //Parse Chapter List
+            MatchCollection chapterMatches = Regex.Matches(response, @"(https.*?)\.data\.(.*?)\.data\.(.*?)\.data\.(available|locked)\.end_data\.");
+            foreach (Match chapterMatch in chapterMatches)
+            {
+                string chapterTitle = System.Net.WebUtility.HtmlDecode(chapterMatch.Groups[2].Value);
+                yield return new Chapter(chapterMatch.Groups[1].Value, chapterTitle);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Insert the novel link from CreativenNovels");
+            var link = Console.ReadLine();
+            var response = GetChapters(link).ToArray();
+           
+            for (i = 0; i < response.Length; ++i)
+                Console.WriteLine($"ID: [{i}] {response[i].Title}");
+
+            Console.WriteLine("Select Chapter ID for save the chapter");
+            Console.WriteLine("Selec any invalid ID to exit");
+
+            do
+            {
+                i = Convert.ToInt32(Console.ReadLine());
+                response[i].FillContentAndParse();
+                File.WriteAllText(response[i].Slug + ".html", response[i].Content);
+
+            }
+            while (i >= 0 && i < response.Length);
+        }
+    }
+}
